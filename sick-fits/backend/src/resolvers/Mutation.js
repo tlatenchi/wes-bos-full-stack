a08891fc98e7ac85c974c4034c3867caf7b0b801
Walter Tlatenchi <walter.tlatenchi@revolve.com>
2020-01-15 13:16:05 -0800

    user signup and permission flow - video 24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -40,12 +43,39 @@ const Mutations = {
     // 1. find the item
     // There are times where you need to perform a second query
     // in that case you want to hardcode the query instead of
-    // passing the info object
+    // passing the info object, as the second argument we are saying
+    // we want the id and title back
     const item = await ctx.db.query.item({ where }, `{ id title}`);
     // 2. Check if they own that item, or have the permissions
     // TODO
     // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // Finalllllly we return the user to the browser
+    return user;
   }
 };
 
