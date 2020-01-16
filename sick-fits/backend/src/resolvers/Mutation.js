556836e7d25efca7a40a952f59559491967ad17d
Walter Tlatenchi <walter.tlatenchi@gmail.com>
2020-01-15 21:34:06 -0800

    currently logged in user with middleware and renderprops, sign-in form and custom error handling, signout  - video 26, 27, 28

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -14,20 +14,16 @@ const Mutations = {
       info
     );
 
+    console.log(item);
+
     return item;
   },
   updateItem(parent, args, ctx, info) {
     // first take a copy of the updates
     const updates = { ...args };
-
-    // remove the Id from the updates because
-    // it is not something we should be updating
+    // remove the ID from the updates
     delete updates.id;
-
     // run the update method
-    // look at `prisma.graphql` for available functions
-    // there you'll find `updateItem()` with the
-    // arguments that it needs
     return ctx.db.mutation.updateItem(
       {
         data: updates,
@@ -41,10 +37,6 @@ const Mutations = {
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. find the item
-    // There are times where you need to perform a second query
-    // in that case you want to hardcode the query instead of
-    // passing the info object, as the second argument we are saying
-    // we want the id and title back
     const item = await ctx.db.query.item({ where }, `{ id title}`);
     // 2. Check if they own that item, or have the permissions
     // TODO
@@ -76,6 +68,31 @@ const Mutations = {
     });
     // Finalllllly we return the user to the browser
     return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid Password!');
+    }
+    // 3. generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // 5. Return the user
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
   }
 };
 
