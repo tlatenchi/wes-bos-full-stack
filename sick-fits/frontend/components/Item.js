f2590243b25ad1b1d9cc8ee2175fe750531e4b9a
Walter Tlatenchi <walter.tlatenchi@gmail.com>
2020-01-18 16:14:13 -0800

    server side add to cart - video 40

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -6,8 +6,13 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
+import AddToCart from './AddToCart';
 
-class Item extends Component {
+export default class Item extends Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
+
   render() {
     const { item } = this.props;
     return (
@@ -26,25 +31,20 @@ class Item extends Component {
         </Title>
         <PriceTag>{formatMoney(item.price)}</PriceTag>
         <p>{item.description}</p>
-        <div className='buttonList'>
+
+        <div className="buttonList">
           <Link
             href={{
               pathname: 'update',
               query: { id: item.id }
             }}
           >
-            <a>Edit</a>
+            <a>Edit ✏️</a>
           </Link>
-          <button>Add To Cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
   }
 }
-
-Item.propTypes = {
-  item: PropTypes.object.isRequired
-};
-
-export default Item;
